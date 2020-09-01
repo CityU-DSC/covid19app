dda51077b59f31590f41cba1f22ba546111f72cf
Lukehsu1999 <lukehsu1999@gmail.com>
2020-09-01 21:13:27 +0800

    feature expand version

--- lib/screens/worldList/components/CountryData.dart
+++ lib/screens/worldList/components/CountryData.dart
@@ -2,6 +2,8 @@ import 'package:http/http.dart';
 import 'dart:convert';
 import 'package:intl/intl.dart';
 
+import 'dart:async';
+
 class CountryData {
   String location;
   String loc_url; //url to the world time api
@@ -11,14 +13,35 @@ class CountryData {
       from_date; // https://api.dart.dev/stable/2.9.2/dart-core/DateTime/toIso8601String.html
   String
       to_date; // https://api.dart.dev/stable/2.9.2/dart-core/DateTime/toIso8601String.html
-  int total_cases;
-  int total_deaths;
-  int total_recovery;
-  int total_active;
+  String total_cases;
+  String total_deaths;
+  String total_recovery;
+  String total_active;
+  int tries = 0;
+
+  static dynamic allData = null;
+  static DateTime previousCalledDate = null;
+
+  static Future<void> loadAll() async {
+    if (allData != null && previousCalledDate.day == (new DateTime.now().day)) {
+      return;
+    }
+    try {
+      String full_url =
+          'https://api.covid19api.com/summary';
+      Response response = await get(full_url);
+      allData = jsonDecode(response.body.substring(1, response.body.length - 2));
+      previousCalledDate = new DateTime.now();
+    } catch (e) {
+      print('Caught error: $e');
+    }
+  }
+
 
   CountryData({this.location, this.loc_url});
 
   Future<void> getData() async {
+    tries++;
     date = new DateTime.now();
 
     from_date = date.add(new Duration(days: -1)).toIso8601String();
@@ -26,25 +49,27 @@ class CountryData {
 
     from_date = from_date.substring(0, 11) + '00:00:00Z';
     to_date = to_date.substring(0, 11) + '00:00:00Z';
-    
+
     try {
       //make request
       //'https://api.covid19api.com/total/country/($url)?from=2020-08-28T00:00:00Z&to=2020-08-28T23:00:00Z';
-      String full_url = 'https://api.covid19api.com/total/country/$loc_url?from=$from_date&to=$to_date';
+      String full_url =
+          'https://api.covid19api.com/total/country/$loc_url?from=$from_date&to=$to_date';
       Response response = await get(full_url);
       print('FULL URL: $full_url');
-      Map data = jsonDecode(response.body.substring(1,response.body.length-2));
-      
-      
+      Map data =
+          jsonDecode(response.body.substring(1, response.body.length - 2));
+
       total_cases = data['Confirmed'];
       total_deaths = data['Deaths'];
-      total_recovery=data['Recovered'];
-      total_active=data['Active'];
-
-  
-
+      total_recovery = data['Recovered'];
+      total_active = data['Active'];
     } catch (e) {
       print('Caught error: $e');
+
+      if (tries <= 10){
+        await new Future.delayed(Duration(seconds: tries), () => this.getData());
+      }
     }
   }
 }
@@ -52,7 +77,11 @@ class CountryData {
 //CountryData instance = CountryData(location: 'The US', url: 'usa');
 
 void main() async {
-  CountryData instance = CountryData(location: 'The US', loc_url: 'usa');
+  CountryData.loadAll();
+  print(CountryData.allData);
+
+
+/*  CountryData instance = CountryData(location: 'The US', loc_url: 'usa');
   print(instance.location);
   await instance.getData();
   print(instance.total_cases);
@@ -61,7 +90,6 @@ void main() async {
   print(instance.total_recovery);
 
   print('${instance.from_date} - ${instance.to_date}');
-  
-
+  */
 }
 // dart CountryData.dart;
--- lib/screens/worldList/worldListPage.dart
+++ lib/screens/worldList/worldListPage.dart
@@ -1,6 +1,9 @@
+import 'package:expandable/expandable.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'components/CountryData.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:covid19app/constant.dart';
 
 class WorldListPage extends StatefulWidget {
   @override
@@ -29,6 +32,15 @@ class _WorldListPageState extends State<WorldListPage> {
     CountryData(location: 'Spain', loc_url: 'spain'),
   ];
 
+  bool receivedAll() {
+    for(int i=0; i<locations.length; i++) {
+      if (locations[i].total_cases == 'loading...') {
+        return false;
+      }
+      return true;
+    }
+  }
+
   void printCountryData(index) async{
     CountryData instance = locations[index];
     print(instance.location);
@@ -39,44 +51,82 @@ class _WorldListPageState extends State<WorldListPage> {
     print(instance.total_recovery);
   }
 
-/*
-  void updateData(index) async {
-    WorldTime instance = locations[index];
-    await instance.getTime();
-    //navigate to home screen
-    Navigator.pop(context, {
-      'location':instance.location,
-      'flag':instance.flag,
-      'time':instance.time,
-      'isDaytime':instance.isDayTime,
-    });
-
+  void loadCountryData(index) async {
+    CountryData instance = locations[index];
+    await instance.getData();
+    setState(() {});
   }
-  */
 
   @override
   Widget build(BuildContext context) {
     print('build start');
 
+    // SORT LIST
+    locations.sort((a,b) => a.location.compareTo(b.location));
+
+    // LOAD ALL DATA (IF HAVEN'T ALREADY)
+    if (!receivedAll()) {
+      for(int i=0; i<locations.length; i++) {
+        loadCountryData(i);
+      }
+      //loadCountryDataAll();
+    }
+
     return Scaffold(
       backgroundColor: Colors.grey,
       appBar:AppBar(
-        backgroundColor: Colors.blue,
-        title: Text('Choose a location'),
-        centerTitle: true,
+        elevation: 0,
+        leading: IconButton(
+          icon: SvgPicture.asset(
+            'assets/icons/dsc_icon.svg',
+            color: Colors.white,
+          ),
+          onPressed: (){
+            Navigator.pop(context);
+          },
+        ),
+        title: Text(
+          'Choose A Location',
+          style: TextStyle(
+            color: kBodyTextColor,
+            fontSize: 15,
+            fontWeight: FontWeight.w100,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: SvgPicture.asset('assets/icons/menu.svg'),
+            onPressed: (){
+              Navigator.pop(context);
+            },
+          )
+        ],
       ),
       body: ListView.builder(
         itemCount: locations.length,
         itemBuilder:(context, index){
-          return Padding(
-            padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
-            child: Card(
-              child: ListTile(
-                onTap:(){
-                  print(locations[index].location);
-                  printCountryData(index);
-                },
-                title: Text(locations[index].location),
+          return Container(
+            padding: EdgeInsets.all(8),
+            margin: EdgeInsets.fromLTRB(0, 0, 0, 5),
+            color: Colors.white,
+            child: ExpandablePanel(
+              header: Text(
+                locations[index].location,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              collapsed: Text('Total Confirmed Cases: ' + locations[index].total_cases.toString()),
+              expanded: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Total Confirmed Cases: ' + locations[index].total_cases),
+                  Text('Total Deaths: ' + locations[index].total_deaths),
+                  Text('Total Recoveries: ' + locations[index].total_recovery),
+                  Text('Total Active Cases: ' + locations[index].total_active),
+                ],
+
               ),
             ),
           );
@@ -85,37 +135,3 @@ class _WorldListPageState extends State<WorldListPage> {
     );
   }
 }
-/*
-List<CountryData> locations = [
-    CountryData(location: 'The US', loc_url: 'usa'),
-    CountryData(location: 'Canada', loc_url: 'canada'),
-    CountryData(location: 'Mexico', loc_url: 'mexico'),
-    CountryData(location: 'Argentina', loc_url: 'argentina'),
-    CountryData(location: 'Chile', loc_url: 'chile'),
-    CountryData(location: 'Taiwan', loc_url: 'taiwan'),
-    CountryData(location: 'China', loc_url: 'china'),
-    CountryData(location: 'Japan', loc_url: 'japan'),
-    CountryData(location: 'South Korea', loc_url: 'south-korea'),
-    CountryData(location: 'South Africa', loc_url: 'south-africa'),
-    CountryData(location: 'UK', loc_url: 'uk'),
-    CountryData(location: 'Denmark', loc_url: 'denmark'),
-    CountryData(location: 'Germany', loc_url: 'germany'),
-    CountryData(location: 'France', loc_url: 'france'),
-    CountryData(location: 'Portugal', loc_url: 'portugal'),
-    CountryData(location: 'Spain', loc_url: 'spain'),
-  ];
-
-  void printCountryData(index) async{
-    CountryData instance = locations[index];
-    print(instance.location);
-    await instance.getData();
-    print(instance.total_cases);
-    print(instance.total_deaths);
-    print(instance.total_active);
-    print(instance.total_recovery);
-  }
-
-void main() async {
-  printCountryData(0);
-}
-*/
